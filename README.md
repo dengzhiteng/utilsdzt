2ec227db3665fa16951ceb1b94e4d816e4872455
dengzhiteng <dengzhiteng@jwx.com.cn>
2021-08-11 17:34:41 +0800

    引入rollup 并成功发布

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 ### 对所有的模块进行单位测试 jest
 
 ### 对模块进行打包
+
+- rollup-plugin-node-resolve —帮助 Rollup 查找外部模块，然后导入
+- rollup-plugin-commonjs —将 CommonJS 模块转换为 ES2015 供 Rollup 处理
+- rollup-plugin-babel — 让我们可以使用 es6 新特性来编写代码
+- rollup-plugin-terser — 压缩 js 代码，包括 es6 代码压缩
+- rollup-plugin-eslint — js 代码检测
---
+++ src/ajax/index.js
@@ -0,0 +1,86 @@
+/**
+ *
+ * @param {*} option
+ */
+export function ajax(option) {
+  //1.创建对象
+  var xhr = new XMLHttpRequest();
+  //2.设置请求行(get请求数据写在url后面)
+  // 如果是get请求 并且有数据
+  if (option.type == "get" && option.data) {
+    option.url += "?";
+    option.url += option.data;
+    // 直接设置为null 方便后续使用
+    option.data = null;
+  }
+  xhr.open(option.type, option.url);
+  //3.设置请求头(get请求可以省略,post不发送数据也可以省略)
+  if (option.type == "post" && option.data) {
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  }
+  //3.5注册回调函数
+  xhr.onload = function () {
+    // 增加判断类型的代码
+    var type = xhr.getResponseHeader("content-type");
+    // 说明是xml
+    if (type.indexOf("xml") != -1) {
+      option.success(xhr.responseXML);
+    } else if (type.indexOf("json") != -1) {
+      // 如果是json
+      option.success(JSON.parse(xhr.responseText));
+    } else {
+      // 如果都不是
+      option.success(xhr.responseText);
+    }
+  };
+  //4.请求主体发送(get请求为空，或者写null，post请求数据写在这里，如果没有数据，直接为空或者写null)
+  xhr.send(option.data);
+}
+/**
+ * 封装一个原生的post 请求
+ * @param {*} url
+ * @param {*} data
+ * @param {*} success
+ */
+export function post(url, data, success) {
+  //1.创建对象success) {
+  //1.创建对象
+  var xhr = new XMLHttpRequest();
+  //2.设置请求行(get请求数据写在url后面)
+  xhr.open("post", url);
+  //3.设置请求头(get请求可以省略,post不发送数据也可以省略)
+  if (data) {
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  }
+  //3.5注册回调函数
+  xhr.onload = function () {
+    // console.log(xhr.responseText);
+    success(xhr.responseText);
+  };
+  //4.请求主体发送(get请求为空，或者写null，post请求数据写在这里，如果没有数据，直接为空或者写null)
+  xhr.send(data);
+}
+/**
+ * 封装原生请求
+ * @param {*} url
+ * @param {*} data
+ * @param {*} success
+ */
+export function get(url, data, success) {
+  //1.创建对象
+  var xhr = new XMLHttpRequest();
+  //2.设置请求行(get请求数据写在url后面)
+  if (data) {
+    url += "?";
+    url += data;
+  }
+  xhr.open("get", url);
+  //3.设置请求头(get请求可以省略,post不发送数据也可以省略)
+  //3.5注册回调函数
+  xhr.onload = function () {
+    // console.log(xhr.responseText);
+    success(xhr.responseText);
+  };
+  //4.请求主体发送(get请求为空，或者写null，post请求数据写在这里，如果没有数据，直接为空或者写null)
+  xhr.send(null);
+}
