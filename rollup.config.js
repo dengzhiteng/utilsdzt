bd4954fb243a965373a5703b071fa524f261e6b2
dengzhiteng <dengzhiteng@jwx.com.cn>
2021-08-11 17:58:03 +0800

    进行必要的压缩

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,11 @@
-import commonjs from "rollup-plugin-commonjs";
 import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import babel from "rollup-plugin-babel";
+
+import { terser } from "rollup-plugin-terser";
+import clear from "rollup-plugin-clear"; // 清空文件夹插件
+import { uglify } from "rollup-plugin-uglify"; // 输出libary代码压缩
+
 export default {
   input: "./index.js",
   output: {
@@ -9,33 +15,15 @@ export default {
   },
   plugins: [
     resolve(),
-    commonjs({
-      // non-CommonJS modules will be ignored, but you can also
-      // specifically include/exclude files
-      include: "node_modules/**", // Default: undefined
-      exclude: ["node_modules/foo/**", "node_modules/bar/**"], // Default: undefined
-      // these values can also be regular expressions
-      // include: /node_modules/
-
-      // search for files other than .js files (must already
-      // be transpiled by a previous plugin!)
-      extensions: [".js", ".coffee"], // Default: [ '.js' ]
-
-      // if true then uses of `global` won't be dealt with by this plugin
-      ignoreGlobal: false, // Default: false
-
-      // if false then skip sourceMap generation for CommonJS modules
-      sourceMap: true, // Default: true
-
-      // explicitly specify unresolvable named exports
-      // (see below for more details)
-      namedExports: { react: ["createElement", "Component"] }, // Default: undefined
-
-      // sometimes you have to leave require statements
-      // unconverted. Pass an array containing the IDs
-      // or a `id => boolean` function. Only use this
-      // option if you know what you're doing!
-      ignore: ["conditional-runtime-dependency"],
+    commonjs(),
+    terser(),
+    clear({
+      targets: ["dist"],
+    }),
+    uglify(),
+    babel({
+      exclude: "node_modules/**", // 只编译我们的源代码
+      runtimeHelpers: true,
     }),
   ],
   //告诉rollup不要将此dayjs打包，而作为外部依赖，
